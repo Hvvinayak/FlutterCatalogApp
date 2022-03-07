7f61c529fe1de8d85c62e08ab7dda031ca8e1990
unknown <hvvinayakhimanshu244@gmail.com>
2022-03-07 22:33:28 +0530

    Commit on day10

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:catalog_app/pages/home_page.dart';
 import 'package:catalog_app/pages/login_page.dart';
 import 'package:catalog_app/utils/routes.dart';
+import 'package:catalog_app/widgets/themes.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,10 +17,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       // home: const HomePage(),
       themeMode: ThemeMode.light,
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        fontFamily: GoogleFonts.lato().fontFamily,
-      ),
+      theme: MyTheme.themeData,
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
